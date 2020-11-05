771154a08ea16c73c17f145aa42f5e48950a44cc
hhassan <hassanhamza0101@gmail.com>
2020-11-05 16:00:00 +0500

    Extra columns removed from User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201102134438) do
+ActiveRecord::Schema.define(version: 20201105105439) do
 
   create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.float "total_amount", limit: 24
@@ -44,11 +44,7 @@ ActiveRecord::Schema.define(version: 20201102134438) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.string "remember_digest"
     t.boolean "admin", default: false
-    t.string "activation_digest"
-    t.boolean "activated", default: false
-    t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
