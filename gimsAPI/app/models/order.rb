fa1ef164be6a489c8708ab00ed82f7244ee5f79a
hhassan <hassanhamza0101@gmail.com>
2020-12-06 02:43:48 +0500

    Repo made solely for the frontend.

--- gimsAPI/app/models/order.rb
+++
@@ -1,6 +0,0 @@
-class Order < ApplicationRecord
-	belongs_to :user
-	#belongs_to :product
-	#or
-	#had_many :products
-end
--- gimsAPI/app/models/product.rb
+++
@@ -1,9 +0,0 @@
-class Product < ApplicationRecord
-	belongs_to :user
-
-	validates :name,  presence: true, length: { maximum: 50 }
-	
-	validates :description, length: { maximum: 500 }
-	validates :price, presence: true
-	validates :min_amount, presence: true 
-end
